99e3715a79b8567e57ac8f605f5cae1c22d3353c
Александр Шкаликов <ash@almanitrus.ru>
2022-09-28 22:29:54 +0300

    advances

---
+++ pages/advances — копия/Advances.jsx
@@ -0,0 +1,80 @@
+import { useState, useEffect } from 'react';
+//nextjs
+import styles from './Advances.module.scss'
+// import styles2 from './Feedback.scss'
+import Image from 'next/image'
+//mui
+import { Box, Container, TextField } from '@mui/material';
+import Grid from '@mui/material/Unstable_Grid2';
+//swiper
+import { Swiper, SwiperSlide } from 'swiper/react';
+import { Pagination } from "swiper";
+import 'swiper/css';
+import "swiper/css/pagination";
+//own
+import {Button} from '../../components/atom/button/Button'
+import {Input} from '../../components/atom/input/Input'
+import {Title} from '../../components/atom/title/Title'
+// import {ScheduleRatesCard} from './cmpScheduleRatesCard/ScheduleRatesCard'
+
+//assets
+import bgImage from './assets/bg.jpg'
+
+function СmpGRID(props){
+	const {title, text} = {...props}
+	return (
+		<Grid item xs={12} md={6} className={styles.CmpGrid}>
+			<div className={styles.cell}>
+				<div className={styles.title}>{title}</div>
+				<div className={styles.text}>{text}</div>
+			</div>
+		</Grid>
+	)
+}
+
+export function Advances(){
+	return (
+		<section className={styles.wrapper}>
+			<Container className={styles.container}>
+				<Title 
+				title="Чем хорошее резюме отличаетсяот собранного за 30 минут «на коленке»" 
+				isUnderline={true}
+				/>
+
+				<Grid container xs={12} sx={{padding: '64px 0'}} >
+
+					{/* LEFT */}
+	        <Grid item xs={12} md={6} >
+						<Grid container>
+							<СmpGRID 
+							title="Общее впечатление" 
+							text="Легкость чтения. Аккуратность. Отсутствие грамматических ошибок. Корректный формат" />
+							<СmpGRID 
+							title="Структура" 
+							text="Объем не более 2-х страниц. Информация интуитивно понятна, без визуального шума, ничего лишнего. Сначала важное – навыки, опыт, затем все остальное" />
+							<СmpGRID 
+							title="Содержание" 
+							text="Описаны не только процессы и обязанности, а результаты в цифрах" />
+							<СmpGRID 
+							title="Релевантность вакансии" 
+							text="Контент резюме адаптирован под конкретную должность / роль / индустрию – говорит с нанимателем на одном языке" />
+	        	</Grid>
+	        </Grid>
+					{/* LEFT end */}
+
+
+
+					{/* RIGHT */}
+					<Grid item xs={12} md={6} className={styles.banner}>
+						<Image src={bgImage} layout="intrinsic" />
+	        </Grid>
+					{/* RIGHT end */}
+
+
+        </Grid>
+
+				{/* <Image src={telegram} layout="intrinsic" /> */}
+			</Container>
+		</section>
+	)
+} 
