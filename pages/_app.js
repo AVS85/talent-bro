981073451a31d2f71aade76db2a47dc53b7e8b84
Alexander <seo.resait@ya.ru>
2022-10-08 20:07:00 +0300

    add favicon

--- pages/_app.js
+++ pages/_app.js
@@ -1,8 +1,17 @@
 import '../styles/globals.scss'
+import Head from 'next/head';
 
 function MyApp({ Component, pageProps }) {
   return (
-		<Component {...pageProps} />
+		<div>
+			<Head>
+				<link rel="apple-touch-icon" sizes="180x180" href="/favicon/apple-touch-icon.png" />
+				<link rel="icon" type="image/png" sizes="32x32" href="/favicon/favicon-32x32.png" />
+				<link rel="icon" type="image/png" sizes="16x16" href="/favicon/favicon-16x16.png" />
+				<link rel="manifest" href="/favicon/site.webmanifest"></link>
+			</Head>
+			<Component {...pageProps} />
+		</div>
 	)
 }
 
